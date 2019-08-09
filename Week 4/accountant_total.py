0ca57d2aad756a1e152479766ede3a50205c8df0
Vishal Manghnani <vishal-lollers@hotmail.com>
2019-08-09 19:43:02 +0800

    Week 4 Draft Submission
    Completed all excercises need to double check before LearnJCU submission

---
+++ Week 4/accountant_total.py
@@ -0,0 +1,26 @@
+
+def get_income_and_total(months):
+    income = []
+    total = []
+    curr_total = 0
+    for i in range(months):
+        print("Enter income for month",i+1,": ")
+        curr_income = float(input())
+        income.append(curr_income)
+        curr_total += curr_income
+        total.append(curr_total)
+
+    return (income,total)
+
+
+
+def main():
+    months = int(input("How many months? "))
+    income,total = get_income_and_total(months)
+    print("Income Report")
+    print("----------------")
+    for i in range(months):
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(i+1, income[i], total[i]))
+
+if __name__ == "__main__":
+    main()
